373c4b06d3d26444597540461f354efdb16e6b15
Deepankar Agrawal <deepankar11093@gmail.com>
2019-04-13 00:13:39 +0530

    add file system isolation

--- src/container.rs
+++ src/container.rs
@@ -1,6 +1,14 @@
 use nix::sys::wait::*;
 use nix::unistd::*;
 
+const ROOT_IMAGE_PATH: &str = "/media/deepankar/7039DF1C0BF3397F/Projects/coal/alpine/";
+
+
+fn setup_root(root_path: &str) {
+    chroot(root_path).expect("error called");
+    chdir("/").expect("change directory failed");
+}
+
 pub fn create(command: &Fn(Vec<String>), args: Vec<String>) {
     match fork().expect("fork failed") {
         ForkResult::Parent{ child } => {
@@ -10,6 +18,7 @@ pub fn create(command: &Fn(Vec<String>), args: Vec<String>) {
             }
         }
         ForkResult::Child => {
+            setup_root(ROOT_IMAGE_PATH);
             command(args);
         }
     }
